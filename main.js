53cbdc018923d6ec336d120b1859205f1e2f58e4
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-05-08 15:34:18 -0300

    Update main.js

--- main.js
+++ main.js
@@ -6,28 +6,33 @@
     png_quality:90
   });
 
-  camera = document.getElementById("camera");
+//Coloque o id da camera
+  camera = document.getElementById("???");
 
 Webcam.attach( '#camera' );
 
       
-function take_snapshot()
+ //Crie uma função que se chama 'take_snapshot'
+???
 {
-    Webcam.snap(function(data_uri) {
+/* A função Webcam.snap() é um método da biblioteca Webcam.js, que fornece 
+uma interface fácil de usar para acessar a webcam e tirar fotos.
+Adicione essa função abaixo:*/
+    ???(function(data_uri) {
         document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
     });
 }
-
-  console.log('ml5 version:', ml5.version);
+ 
+ /*Adcione o link do seu teachale machine */
   
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
+classifier = ml5.imageClassifier('???/model.json',modelLoaded);
 
-  function modelLoaded() {
-    console.log('Model Loaded!');
-  }
+/*Crie uma função que se chama  'check'
       
-  function check()
+  ???
   {
+  /*essa parte do código é responsável por enviar a imagem capturada pela webcam 
+  para a biblioteca de classificação de imagem e aguardar a resposta.  */
     img = document.getElementById('captured_image');
     classifier.classify(img, gotResult);
   }
@@ -35,10 +40,13 @@ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models
 
 function gotResult(error, results) {
   if (error) {
-    console.error(error);
+  /* Se ocorrer um erro durante a classificação, a função exibirá o erro no console usando o método console.error().*/
+    ???(error);
   } else {
     console.log(results);
     document.getElementById("result_object_name").innerHTML = results[0].label;
-    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    /* . A precisão (confiança) da classificação é definida como o valor da propriedade confidence do mesmo objeto e é 
+    arredondada para três casas decimais usando o método toFixed().*/
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.???(3);
   }
 }
